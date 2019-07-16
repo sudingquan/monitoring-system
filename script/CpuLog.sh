8864677d50bf923dae8f046e01c28cb4a59918f6
sudingquan <1151015256@qq.com>
2019-07-16 21:03:59 +0800

    7-16

--- script/CpuLog.sh
+++ script/CpuLog.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-time=$(date "+%Y-%m-%d__%H:%M:%S");
-min1=$(cat /proc/loadavg | cut -d " " -f 1);
-min5=$(cat /proc/loadavg | cut -d " " -f 2);
-min15=$(cat /proc/loadavg | cut -d " " -f 3);
-t1=0;
-t2=0;
-idle1=`cat /proc/stat | head -1 | cut -d " " -f 6`;
-stat1=`cat /proc/stat | head -1 | awk '{$1="";print $0}'`;
-sleep 0.5;
+time=$(date "+%Y-%m-%d__%H:%M:%S")
+min1=$(cat /proc/loadavg | cut -d " " -f 1)
+min5=$(cat /proc/loadavg | cut -d " " -f 2)
+min15=$(cat /proc/loadavg | cut -d " " -f 3)
+t1=0
+t2=0
+idle1=`cat /proc/stat | head -1 | cut -d " " -f 6`
+stat1=`cat /proc/stat | head -1 | awk '{$1="";print $0}'`
+sleep 0.5
 idle2=`cat /proc/stat | head -1 | cut -d " " -f 6`
-stat2=`cat /proc/stat | head -1 | awk '{$1="";print $0}'`;
+stat2=`cat /proc/stat | head -1 | awk '{$1="";print $0}'`
 
 for i in ${stat1}; do
-    t1=$[ ${t1} + ${i} ];
+    t1=$[ ${t1} + ${i} ]
 done
 for i in ${stat2}; do
-    t2=$[ ${t2} + ${i} ];
+    t2=$[ ${t2} + ${i} ]
 done
-idle=$[ ${idle2} - ${idle1} ];
-totalCpuTime=$[ ${t2} - ${t1} ];
+idle=$[ ${idle2} - ${idle1} ]
+totalCpuTime=$[ ${t2} - ${t1} ]
 usage=$(awk -v x=${idle} -v y=${totalCpuTime} 'BEGIN {printf "%.2f\n",100*(y-x)/y}')
-true_temp=$(cat /sys/class/thermal/thermal_zone0/temp);
+true_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
 temp=$(awk -v x=${true_temp} -v y=1000 'BEGIN {printf "%.2f\n",x/y}')
 if [[ ${true_temp} -lt 50000 ]]; then
-    alert="nolmal";
+    alert="nolmal"
 elif [[ ${true_temp} -lt 70000 ]]; then
-    alert="note";
+    alert="note"
 else
-    alert="warning";
+    alert="warning"
 fi
-echo ${time} ${min1} ${min5} ${min15} ${usage}"%" ${temp}"℃" ${alert};
+echo "${time} ${min1} ${min5} ${min15} ${usage}"%" ${temp}"℃" ${alert}"
---
+++ script/MemLog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+time=$(date "+%Y-%m-%d__%H:%M:%S");
+DyAver=$1
+if [[ ${DyAver}x == "x" ]]; then
+    exit 1
+fi
+MemValue=(`free -m | head -2 | tail -1 | awk '{printf("%d %d", $2, $3)}'`)
+total=${MemValue[0]}
+used=${MemValue[1]}
+left=$[ ${total} - ${used} ]
+MemAvaPrec=`echo "scale=1;${used}*100/${total}" | bc`
+DyAver=`echo "scale=1;${DyAver}*0.3+${MemAvaPrec}*0.7" | bc`
+echo "${time} ${total}M ${left}M ${MemAvaPrec}% ${DyAver}%";
