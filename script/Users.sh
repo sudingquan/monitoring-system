e41b486ad122be16e5cf44e08fdcafc78633c041
sudingquan <1151015256@qq.com>
2019-07-17 01:46:18 +0800

    7-17

---
+++ script/Users.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+time=$(date "+%Y-%m-%d__%H:%M:%S")
+UserNum=$(grep -v /sbin/nologin /etc/passwd | cat -b | tail -1 | awk '{printf("%d",$1)}')
+ActUser=$(last | head -n -2 | awk '{printf("%s\n", $1)}' | uniq | head -3 | tr "\n" ",")
+RootUser=$(cat /etc/group | grep sudo | cut -d ":" -f 4)
+OnlineUser=$(w | tail -n +3 | awk '{printf("%s_%s_%s,", $1, $3, $2)}')
+
+echo "${time} ${UserNum} [${ActUser%,}] [${RootUser}] [${OnlineUser%,}]"
